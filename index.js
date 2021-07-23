d331355516e6021b5fa2e403ff7b298ef706f98e
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-07-23 14:08:07 +0530

    add findOneAndUpdate

--- index.js
+++ index.js
@@ -277,15 +277,21 @@ Parameters            tisbn
 Method                PUT
 */
 
-shapeAI.put("/book/update/:isbn", (req, res) => {
-    database.books.forEach((book) => {
-        if (book.ISBN === req.params.isbn) {
-            book.title = req.body.bookTitle;
-            return;
-        }
-    });
+shapeAI.put("/book/update/:isbn", async (req, res) => {
 
-    return res.json({ books: database.books });
+    const updatedBook = await BookModel.findOneAndUpdate(
+        {
+            ISBN: req.params.isbn,
+        },
+        {
+            title: req.body.bookTitle,
+        },
+        {
+            new: true,
+        }
+    );
+
+    return res.json({ books: updatedBook });
 });
 
 /*
